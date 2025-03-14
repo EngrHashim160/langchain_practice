6de6aac0f0c5d731b430525e6da6e72869b8ec66
EngrHashim160 <engrhashim160@gmail.com>
2025-03-14 16:00:19 +0500

    complexity added to the sequential_chains.py

--- LangChain_Chains/sequential_chains.py
+++ LangChain_Chains/sequential_chains.py
@@ -4,19 +4,24 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 
-prompt = PromptTemplate(
-    template="Generate 5 interesting facts about {topic}",
+prompt1 = PromptTemplate(
+    template="Generate a detail report on {topic}",
     input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template="Pitch the 5 pointer summary from the report {report}.",
+    input_variables=['report']
 )
 
 model = ChatGroq(model="mixtral-8x7b-32768")
 
 parser = StrOutputParser()
 
-chain = prompt | model | parser
+chain = prompt1 | model | parser | prompt2 | model | parser
 
-response = chain.invoke({'topic': 'Gen AI'})
+response = chain.invoke({'topic': 'Jobs Shortage in Pakistan'})
 
 print(response)
 
-chain.get_graph().print_ascii()
+chain.get_graph().print_ascii() # graphical representation of the chain
