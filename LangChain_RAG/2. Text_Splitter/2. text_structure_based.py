9cc5bdee71fb2bca227532444c7701b9b161856b
EngrHashim160 <engrhashim160@gmail.com>
2025-04-10 00:21:56 +0500

    Text structure based text splitter implemented

---
+++ LangChain_RAG/2. Text_Splitter/2. text_structure_based.py
@@ -0,0 +1,18 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+
+loader = TextLoader('../source/cricket.txt')
+
+docs = loader.load()
+
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap=0,
+)
+
+print(docs[0].page_content)
+
+result = splitter.split_documents(docs)
+
+print(result[0])
